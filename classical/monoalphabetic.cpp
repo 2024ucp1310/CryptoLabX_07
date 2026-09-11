2d81893d0494e12b2fd98a93e0add14fc46d05fb
2024ucp1310 <2024ucp1310@mnit.ac.in>
2026-09-11 11:00:13 +0530

    Wrote Program to convert plaintext to ciphertext using 26 char long key and then storing the output in ciphertext.txt file for later uses(wrote encryptText() function)

---
+++ classical/monoalphabetic.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+string encryptText(string plaintext, string key)
+{
+    string ciphertext = "";
+
+    for (char c : plaintext)
+    {
+        if (isalpha(c))
+        {
+            char upper = toupper(c);
+
+            int index = upper - 'A';
+
+            char encrypted = key[index];
+
+            if (islower(c))
+                encrypted = tolower(encrypted);
+
+            ciphertext += encrypted;
+        }
+        else
+        {
+
+            ciphertext += c;
+        }
+    }
+
+    return ciphertext;
+}
+
+int main()
+{
+
+    ifstream input("../datasets/plaintext.txt");
+
+    if (!input)
+    {
+        cout << "Cannot open plaintext.txt" << endl;
+        return 1;
+    }
+
+    string plaintext;
+    string line;
+
+    while (getline(input, line))
+    {
+        plaintext += line + "\n";
+    }
+
+    input.close();
+
+
+    string key;
+
+    cout << "Enter 26-letter substitution key: ";
+    cin >> key;
+
+
+    for (char &c : key)
+        c = toupper(c);
+
+
+    if (key.length() != 26)
+    {
+        cout << "Key must contain exactly 26 letters." << endl;
+        return 1;
+    }
+
+
+    string ciphertext = encryptText(plaintext, key);
+
+
+    ofstream output("../outputs/ciphertext.txt");
+
+    if (!output)
+    {
+        cout << "Cannot create ciphertext.txt" << endl;
+        return 1;
+    }
+
+    output << ciphertext;
+
+    output.close();
+
+    cout << "\nEncryption successful!" << endl;
+    cout << "Ciphertext saved in outputs/ciphertext.txt" << endl;
+
+    return 0;
+}
